2b3097069ecb35c589865b53a8a543fb3bd4a2bc
Your Name <you@example.com>
2021-08-25 04:21:25 +0430

    use wp cli to scaffold plugin

--- README.md
+++
@@ -1,2 +0,0 @@
-# wordpress-security-scanner-plugin
-wordpress plugin for scan your wordpress site and find Vulnerable or security issues
---
+++ pasban-security-scanner.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Pasban Security Scanner
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     pasban-security-scanner
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Pasban_Security_Scanner
+ */
+
+// Your code starts here.
