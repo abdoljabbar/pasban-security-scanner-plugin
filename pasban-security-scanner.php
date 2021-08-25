205c590196ace263ef04b94a23d6f972eaf3c0a6
Your Name <you@example.com>
2021-08-25 04:34:22 +0430

    update plugin header info

--- pasban-security-scanner.php
+++ pasban-security-scanner.php
@@ -1,13 +1,17 @@
 <?php
 /**
- * Plugin Name:     Pasban Security Scanner
- * Plugin URI:      PLUGIN SITE HERE
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Plugin Name:		Pasban Security Scanner
+ * Plugin URI:      https://github.com/abdoljabbar/wordpress-security-scanner-plugin
+ * Description:     Pasban means Gatekeeper, you have many Gates that data comes in your site so you must watch out carefully
+ * Author:          Abdoljabbar Bakhshande
+ * Author URI:      https://stackoverflow.com/users/1611928/abdoljabbar
  * Text Domain:     pasban-security-scanner
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * License:           GPL v3
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
  *
  * @package         Pasban_Security_Scanner
  */
